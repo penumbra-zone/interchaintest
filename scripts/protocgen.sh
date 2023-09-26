ac66c74d4eafa24bb3ac4768af74e1529a6524e1
Conor Schaefer <conor@penumbralabs.xyz>
2023-09-26 12:27:31 -0700

    PARTIAL update proto-gen scripts
    The new paths for penumbra protos, released as part of v0.61.0, will
    write files to different places.

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -4,9 +4,12 @@ set -eox pipefail
 
 buf generate --template proto/buf.gen.penumbra.yaml buf.build/penumbra-zone/penumbra
 
+# Remove previously generated golang proto files, to ensure only newly generated
+# artifacts are included.
+find chain/penumbra -type f -iname '*.pb.go' -exec rm {} +
+
 # move proto files to the right places
 # Note: Proto files are suffixed with the current binary version.
-# rm -r github.com/strangelove-ventures/interchaintest/v*/chain/penumbra/client
 rm -r github.com/strangelove-ventures/interchaintest/v*/chain/penumbra/narsil
 cp -r github.com/strangelove-ventures/interchaintest/v*/* ./
 rm -rf github.com
